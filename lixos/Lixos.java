d01b2847df0ab1b16874666eef2bbf6591a1c51e
Marte <159561648+llMartell@users.noreply.github.com>
2025-11-20 19:09:01 -0300

    Etapa 2 e 3: Criação da Thread Coleta Lixo e suas aplicações

---
+++ lixos/Lixos.java
@@ -0,0 +1,19 @@
+package lixos;
+
+import java.util.Random;
+
+public enum Lixos {
+	PAPEL, PLASTICO, VIDRO, METAL, ORGANICO, NAO_RECICLAVEL;
+
+	public boolean eReciclavel() {
+		return this != NAO_RECICLAVEL;
+	}
+	
+	public static Lixos getLixoAleatorio() {
+		Lixos[] lixos = Lixos.values();
+		
+		int indice = new Random().nextInt(lixos.length);
+		
+		return lixos[indice];
+	}
+}
